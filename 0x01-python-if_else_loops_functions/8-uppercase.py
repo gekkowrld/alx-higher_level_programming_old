fa56d243647f8cc064830fa0ad49258d44a5937d
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-08-10 05:28:02 +0300

    chore: change lowercase to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+place_holder = ''
+def uppercase(str):
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            place_holder = chr(ord(i) - 32)
+        else:
+            place_holder = i
+        print("{}".format(place_holder), end='')
+    print()
