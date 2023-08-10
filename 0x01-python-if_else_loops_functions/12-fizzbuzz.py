5ef85371e6442a2f530755b5c82ad85fff4beda5
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-08-10 06:06:24 +0300

    chore: do a Fizz Buzz work

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,12 @@
 def fizzbuzz():
     for i in range(1, 101):
         response = ''
-        if i % 3 == 0:
-            response += 'Fizz'
-        if i % 5 == 0:
-            response += 'Buzz'
+        if i % 5 == 0 and i % 3 == 0:
+            response = 'FizzBuzz'
+        elif i % 3 == 0:
+            response = 'Fizz'
+        elif i % 5 == 0:
+            response = 'Buzz'
         else:
             response = str(i)
         print("{} ".format(response), end='')
