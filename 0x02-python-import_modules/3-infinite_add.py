d2f1a1b52b7cffd51af3bf53036a278ae986fd08
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-08-11 19:35:43 +0300

    chore: Add the cli arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    result = 0
+    for i in argv:
+        if argv.index(i) == 0:
+            continue
+        result += int(i)
+
+    print(result)
