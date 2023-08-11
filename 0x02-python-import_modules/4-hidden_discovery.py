ef64900dcdf07173540bf82fb2dd630c453de7fe
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-08-11 21:45:41 +0300

    chore: Get those mystery goodies in a discovery journey

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import importlib
+    modules = importlib.import_module("hidden_4")
+
+    func_names = [name for name in dir(modules) if not name.startswith('__')]
+    func_names.sort()
+
+    for func_name in func_names:
+        print(func_name)
