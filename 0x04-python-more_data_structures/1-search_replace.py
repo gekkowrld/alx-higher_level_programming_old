16b2dd8c705d7d5c7429e17674408f23332bed8d
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-08-18 20:09:40 +0300

    fix: just check for equality

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    the_index = new_list.index(search)
-    new_list[the_index] = replace
-
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
