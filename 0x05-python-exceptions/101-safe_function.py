08a77e9c37278d7e4b3f644fd33cedea947ae336
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-08-29 00:03:58 +0300

    fix: Use optimized, 'pythonic' way of dealing with errors
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,9 +5,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        a, b = args
-        result = fct(a, b)
+        result = fct(*args)
         return result
-    except (RuntimeError, TypeError, ValueError,
-            ZeroDivisionError, IndexError) as e:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
