579297fef6e22ed6617ee40f24562eb4d6bd0f68
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-08-30 02:30:20 +0300

    fix: Use the correct printing format
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(0, len(matrix[0])):
         for j in range(0, len(matrix[1])):
-            print("{}".format(matrix[i][j]), end="")
+            print("{:d}".format(matrix[i][j]), end="")
             if (j + 1) < len(matrix[1]):
                 print(" ", end="")
         print()
