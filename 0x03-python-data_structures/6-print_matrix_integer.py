9c5f6da5d02a06b89219d34b429676fa9d4496f8
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-08-30 02:28:04 +0300

    chore: Print a matrix
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(0, len(matrix[0])):
+        for j in range(0, len(matrix[1])):
+            print("{}".format(matrix[i][j]), end="")
+            if (j + 1) < len(matrix[1]):
+                print(" ", end="")
+        print()
