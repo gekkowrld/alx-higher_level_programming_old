51e1d2adcb120fe55e6cc6da036864614b76b1f7
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-08-30 01:30:50 +0300

    chore: Print '#' to the stdout for every square
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""Create an empty class that does nothing"""
+
+
+class Square:
+    """Class Square
+
+    This class deals with squares
+    """
+    def __init__(self, size=0):
+        """Create the square attributes
+
+        Args:
+          size (int): The size of the square to be created
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get and or set the values of square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Return the value of the area of the square
+
+        Since a square is the, well multiplication of two numbers, then
+          we multiply the two
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print '#' to the stdout
+        Print the square where size is more than 0, else
+            print an empty line
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for i in range(0, self.__size):
+                    print('#', end='')
+                print()
