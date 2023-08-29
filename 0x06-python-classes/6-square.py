5a88c8e65eebf4c1f4d71f5cd8e441e8f3b856ac
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-08-30 01:53:30 +0300

    fix: return
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -61,6 +61,7 @@ class Square:
         """
         if self.__size == 0:
             print()
+            return
         else:
             [print() for i in range(0, self.__position[1])]
             for i in range(0, self.__size):
