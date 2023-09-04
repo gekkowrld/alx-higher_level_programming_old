f8c60162e9a5b83b981db43668111cfaff3b2159
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-09-04 22:38:13 +0300

    chore: Create a class 'Rectangle'
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This module deals with Rectangles
+
+It is empty, for now, it will be filled
+    in subsequent iterations
+"""
+
+
+class Rectangle:
+    """ A class that deals with Rectangles
+    The defination of the rectangle should be done here
+    """
+
+    pass
