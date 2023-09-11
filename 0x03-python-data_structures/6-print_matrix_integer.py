517d3431eba7d933b159dba4982cf2f6c22186fb
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-09-11 19:28:53 +0300

    ff: Sync local and remote branches
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix[0])):
-        for j in range(0, len(matrix[1])):
+    for i in range(0, len(matrix[1])):
+        for j in range(0, len(matrix[0])):
             print("{:d}".format(matrix[i][j]), end="")
             if (j + 1) < len(matrix[1]):
                 print(" ", end="")
