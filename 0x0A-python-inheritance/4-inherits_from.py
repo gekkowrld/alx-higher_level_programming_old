17e52fe849cb5b5eb53c7a5f3b549d05965bbf59
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-09-11 21:41:25 +0300

    chore: check for inheritance from
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+"""
+Check if a value inherits from
+"""
+
+
+def inherits_from(obj, a_class):
+    """Check for inheritance
+
+    obj : object
+        An object that will be evaluated
+
+    a_class : class
+        A class in which the evaluation will be done against
+    """
+
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+
+    return False
