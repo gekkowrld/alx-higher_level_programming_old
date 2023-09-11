14823975f88e6684faa7fe68a5c64b05b153b02e
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-09-11 21:49:33 +0300

    chore: create a base geometry class
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+A geometry class that holds geometry
+    of any description
+
+The baseGeometry class specifies how other geometrical
+    shapes acts
+"""
+
+
+class BaseGeometry:
+    """ Create a base class for geometry"""
+
+    pass
